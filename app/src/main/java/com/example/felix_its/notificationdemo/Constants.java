d9bd275897c70fa73bc4f945f85949d9631db992
Rizwanul Haque <riztech.dev@gmail.com>
2018-07-15 12:11:43 +0530

    added request

--- app/src/main/java/com/example/felix_its/notificationdemo/Constants.java
+++ app/src/main/java/com/example/felix_its/notificationdemo/Constants.java
@@ -4,4 +4,20 @@ public class Constants {
 
     public static final String FIREBASE_TOKEN = "firebase token";
 
+   /* { "data": {
+        "title": "Corona",
+                "message": "Notification testing at Neotech",
+                "img":"https://api.androidhive.info/images/glide/large/deadpool.jpg"
+    },
+        "to" : "efuVCQ0iOzY:APA91bErtFlo8q4dbql6mA9ip9jr08nRjdXgvxnrIFcAKQpSrp2ydlwCr_vlbrEC1qhGbv5gRHZm67Ek55TAdFHnkeJF3X5M2xxNCW1VfKc9cfXiVThN-19Ms7r5n-k_42zjhR5q7X8LuooYGNZlv2wnHBiFOQiRyw"
+    }
+
+
+    key=AAAAJ4qK_C0:APA91bEA0LmPimpu3haqznNC85jNIt2VIs-YVxuXNq5ZkX9Cu4FTLF77Xz-cS8iStwJzin7s3ETIBV82XnGTzvSr5Qcvg-SHhG-sHovhiQoW5J5gxGeFEDDzy8_w7xDyaa-ZAHOjtXlgb5NIGnEaW-zTwx9IZJ208A
+
+    https://fcm.googleapis.com/fcm/send
+*/
+
+
+
 }
--- app/src/main/java/com/example/felix_its/notificationdemo/MainActivity.java
+++ app/src/main/java/com/example/felix_its/notificationdemo/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.felix_its.notificationdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d("Token", "Refreshed token: " + refreshedToken);
     }
 }
